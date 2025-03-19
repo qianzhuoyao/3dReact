b1600905d8ec57203ca8d6e6dc7e354e5cc959f2
1059116864@qq.com <arronqzy@outlook.com>
2025-03-19 16:16:40 +0800

    feat:color set and reset

--- src/plugins/render/checkIntersection.ts
+++ src/plugins/render/checkIntersection.ts
@@ -1,6 +1,28 @@
-import { MeshStandardMaterial } from "three";
+import {
+  BufferGeometry,
+  Material,
+  Mesh,
+  MeshStandardMaterial,
+  NormalBufferAttributes,
+  Object3DEventMap,
+} from "three";
 import { getWindowSingle } from "../../window/windowSingle";
 import { PAUSE_INTERSECTION } from "../../common/constant";
+import { createSingle } from "../../common/createSingle";
+
+const originalMaterials = createSingle(() => {
+  return {
+    origins: new Set<
+      Mesh<
+        BufferGeometry<NormalBufferAttributes>,
+        Material | Material[],
+        Object3DEventMap
+      >
+    >(),
+    materials: new WeakMap(),
+  };
+});
+
 /**
  * 这个要针对模型来
  *
@@ -18,11 +40,27 @@ export const checkIntersection = () => {
     getWindowSingle().threeRaycaster.intersectObjects(
       getWindowSingle().threeScene.children
     );
+  console.log(originalMaterials()?.origins, "ooo");
+  originalMaterials()?.origins.forEach((o) => {
+    const met = Array.isArray(o.material) ? o.material?.[0] : o.material;
+    const om = originalMaterials()?.materials.get(o);
+    if (om) {
+      met.copy(om); // 恢复材质
+      originalMaterials()?.materials.delete(o);
+      originalMaterials()?.origins.delete(o);
+    }
+  });
 
   if (getWindowSingle().threeIntersection.length > 0) {
-    const material = getWindowSingle().threeIntersection[0].object.material;
+    const object = getWindowSingle().threeIntersection[0].object;
+    const material = object.material;
+
     if (material instanceof MeshStandardMaterial) {
-      material.color.set(0xff0000);
+      if (!originalMaterials()?.materials.has(object)) {
+        originalMaterials()?.origins.add(object);
+        originalMaterials()?.materials.set(object, material.clone()); // 克隆原材质
+      }
+      material.emissive.set(0x008000);
     }
   }
 };
